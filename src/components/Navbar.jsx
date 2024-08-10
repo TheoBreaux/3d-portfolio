d7e4d1ff3911e84c6e1849bf9b5281e70eb1616e
Theo Breaux <theobreaux@gmail.com>
2024-08-10 12:49:50 -0700

    under construction for acting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,121 @@
 import { NavLink } from 'react-router-dom';
-import { IoHome } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
     <header className="header">
-      <NavLink
-        to="/"
-        className="text-sm w-10 h-10 rounded-lg  bg-white items-center justify-center flex font-bold shadow-md border border-blue-300"
-      >
-        <div className="col">
-          <p className="blue-gradient_text">TB</p>
-          {/* <IoHome
-            className="blue-gradient"
-            color="#00c6ff"
-            size={20}
-          /> */}
-        </div>
-      </NavLink>
-      <nav className="flex text-lg gap-7 font-medium">
+      <div>
         <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive ? 'text-blue-500' : 'text-black'
-          }
+          to="/"
+          className="text-sm w-10 h-10 mb-4 rounded-lg bg-white items-center justify-center flex font-bold shadow-md border border-blue-300"
         >
-          About
+          <div className="col">
+            <p className="blue-gradient_text">TB</p>
+          </div>
         </NavLink>
-        <NavLink
-          to="/projects"
-          className={({ isActive }) =>
-            isActive ? 'text-blue-500' : 'text-black'
-          }
-        >
-          Projects
-        </NavLink>
-        <NavLink
-          to="/marketing"
-          className={({ isActive }) =>
-            isActive ? 'text-blue-500' : 'text-black'
-          }
-        >
-          Marketing
-        </NavLink>
+      </div>
 
-        {/* <NavLink
-          to="/acting"
-          className={({ isActive }) =>
-            isActive ? 'text-blue-500' : 'text-black'
-          }
-        >
-          Acting
-        </NavLink> */}
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive ? 'text-blue-500' : 'text-black'
-          }
-        >
-          Contact
-        </NavLink>
-      </nav>
+      <div>
+        <nav className="flex text-lg gap-7 font-medium">
+          {['/about', '/projects', '/marketing', '/acting', '/contact'].map(
+            (path) => (
+              <NavLink
+                key={path}
+                to={path}
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? 'text-blue-500'
+                      : location.pathname === '/acting'
+                      ? 'text-yellow-500'
+                      : 'text-black'
+                  } text-base sm:text-lg md:text-xl`
+                }
+              >
+                {path.substring(1).charAt(0).toUpperCase() + path.substring(2)}
+              </NavLink>
+            )
+          )}
+        </nav>
+      </div>
     </header>
   );
 };
 
 export default Navbar;
+
+
+
+// import { NavLink } from 'react-router-dom';
+
+// const Navbar = () => {
+//   return (
+//     <header className="header">
+//       <div>
+//         <NavLink
+//           to="/"
+//           className="text-sm w-10 h-10 mb-4 rounded-lg bg-white items-center justify-center flex font-bold shadow-md border border-blue-300"
+//         >
+//           <div className="col">
+//             <p className="blue-gradient_text">TB</p>
+//           </div>
+//         </NavLink>
+//       </div>
+
+//       <div>
+//         <nav className="flex text-lg gap-7 font-medium">
+//           <NavLink
+//             to="/about"
+//             className={({ isActive }) =>
+//               ${
+//                 isActive ? 'text-blue-500' : 'text-black'
+//               } text-base sm:text-lg md:text-xl
+//             }
+//           >
+//             About
+//           </NavLink>
+//           <NavLink
+//             to="/projects"
+//             className={({ isActive }) =>
+//               ${
+//                 isActive ? 'text-blue-500' : 'text-black'
+//               } text-base sm:text-lg md:text-xl
+//             }
+//           >
+//             Projects
+//           </NavLink>
+//           <NavLink
+//             to="/marketing"
+//             className={({ isActive }) =>
+//               ${
+//                 isActive ? 'text-blue-500' : 'text-black'
+//               } text-base sm:text-lg md:text-xl
+//             }
+//           >
+//             Marketing
+//           </NavLink>
+
+//           <NavLink
+//             to="/acting"
+//             className={({ isActive }) =>
+//               ${
+//                 isActive ? 'text-blue-500' : 'text-black'
+//               } text-base sm:text-lg md:text-xl
+//             }
+//           >
+//             Acting
+//           </NavLink>
+//           <NavLink
+//             to="/contact"
+//             className={({ isActive }) =>
+//               ${
+//                 isActive ? 'text-blue-500' : 'text-black'
+//               } text-base sm:text-lg md:text-xl
+//             }
+//           >
+//             Contact
+//           </NavLink>
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// };
--- src/pages/Acting.jsx
+++ src/pages/Acting.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import UnderConstruction from '../assets/images/under_construction.jpg';
 
 const Acting = () => {
-  return <div>Acting</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <img
+        className="w-full max-w-4xl h-auto"
+        src={UnderConstruction}
+        alt="Under Construction"
+      />
+    </div>
+  );
 };
 
 export default Acting;
